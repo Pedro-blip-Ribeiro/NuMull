6dc84d9d045f2df0ec6e5a6a0986605c07ca5f5a
Pedro Ribeiro <85170807+Pedro-blip-Ribeiro@users.noreply.github.com>
2024-10-14 13:43:46 -0300

    Create NuMul.py

---
+++ NuMul.py
@@ -0,0 +1,13 @@
+# Escreva um script que pergunte um número e escreva a tabuada de multiplicar deste (1 até 10).
+x = int(input('Digite um número: '))
+mltcdr = 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}"); mltcdr += 1
+print(f"{x} x {mltcdr} = {x*mltcdr}", end='\n Acabou!'); mltcdr += 1
